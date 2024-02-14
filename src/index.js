402f490292b1da6574691c1e1d6ff118fa1e2a30
KM <the.real.laplace@gmail.com>
2024-02-14 00:29:38 +0000

    working

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const eslint = require('eslint')
+const ruleComposer = require('eslint-rule-composer')
+
+const makeNeuteredRule = (rule) => ruleComposer
+	.mapReports(rule, (problem) => ({ ...problem, fix: null}))
+
+const init = (plugins) => {
+	const rules = {}
+
+	const builtinRules = new eslint.Linter().getRules()
+	for (const [ ruleName, rule ] of builtinRules.entries()) {
+		rules[ruleName] = makeNeuteredRule(rule)
+	}
+
+	for (const [ pluginName, plugin ] of Object.entries(plugins)) {
+		for (const [ ruleName, rule ] of Object.entries(plugin.rules)) {
+			rules[`${pluginName}/${ruleName}`] = makeNeuteredRule(rule)
+		}
+	}
+
+	return {
+		plugins: { 'no-autofix': { rules } },
+		rules: {},
+	}
+}
+
+module.exports = { init }
