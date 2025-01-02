2253a26ad84f489846f79254fe5a20546b668492
KM <the.real.laplace@gmail.com>
2025-01-02 17:42:13 +0200

    chore(commit): force

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 const eslint = require('eslint')
 const ruleComposer = require('eslint-rule-composer')
+const Fs = require('node:fs')
+const { findUpSync } = require('@kmamal/find-up')
+
+const pkg = JSON.parse(Fs.readFileSync(findUpSync('package.json'), 'utf8'))
+
 
 const makeNeuteredRule = (rule) => ruleComposer
 	.mapReports(rule, (problem) => ({ ...problem, fix: null }))
 
 const init = (plugins) => {
-	const rules = {}
+	const neuteredRules = {}
 
-	const builtinRules = new eslint.Linter().getRules()
+	const builtinRules = new eslint.Linter({ configType: 'eslintrc' }).getRules()
 	for (const [ ruleName, rule ] of builtinRules.entries()) {
-		rules[ruleName] = makeNeuteredRule(rule)
+		neuteredRules[ruleName] = makeNeuteredRule(rule)
 	}
 
 	for (const [ pluginName, plugin ] of Object.entries(plugins)) {
 		for (const [ ruleName, rule ] of Object.entries(plugin.rules)) {
-			rules[`${pluginName}/${ruleName}`] = makeNeuteredRule(rule)
+			neuteredRules[`${pluginName}/${ruleName}`] = makeNeuteredRule(rule)
 		}
 	}
 
 	return {
-		plugins: { 'no-autofix': { rules } },
-		rules: {},
+		plugins: {
+			'no-autofix': {
+				meta: {
+					name: pkg.name,
+					version: pkg.version,
+				},
+				rules: neuteredRules,
+				configs: {},
+				processors: {}
+			}
+		}
 	}
 }
 
